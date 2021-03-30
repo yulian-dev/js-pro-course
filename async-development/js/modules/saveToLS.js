c6659129eb8d00b65bfa6348f0b4d58c8f4dac50
yivanova <yuliya.maslenka@gmail.com>
2021-03-30 14:06:55 +0300

    changes after code review

--- async-development/js/modules/saveToLS.js
+++ async-development/js/modules/saveToLS.js
@@ -16,15 +16,15 @@ export const saveToLocalstorage = (data) => {
 
 const getWeatherObj = (location, current) => {
     return {
-        "name": location.name,
-        "country": location.country,
-        "temperature": current.temperature,
-        "humidity": current.humidity,
-        "pressure": current.pressure,
-        "cloudCover": current.cloudcover,
-        "visibility": current.visibility,
-        "windSpeed": current.wind_speed,
-        "windDir": current.wind_dir,
-        "windDegree": current.wind_degree
+        name: location.name,
+        country: location.country,
+        temperature: current.temperature,
+        humidity: current.humidity,
+        pressure: current.pressure,
+        cloudCover: current.cloudcover,
+        visibility: current.visibility,
+        windSpeed: current.wind_speed,
+        windDir: current.wind_dir,
+        windDegree: current.wind_degree
     }
 }
